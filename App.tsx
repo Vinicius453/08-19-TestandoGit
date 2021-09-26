08c91f153538dd4077690f0bc85d7c0a1776da50
Vinicius Karnopp <viniciuskarnopp777@gmail.com>
2021-09-26 16:51:14 -0400

    Colocando imagens

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+          <View style={styles.header}>
+              <Image source={
+                    require("./src/assets/arrow-left.png")
+                    }>
+                </Image>
+                <Image source={
+                    require("./src/assets/ei_search.png")
+                    }>
+                </Image>
+                <Image source={
+                    require("./src/assets/share.png")
+                    }>
+                </Image>
+
+          </View>
+          <Text>Hello World!</Text>
+          <StatusBar style="auto"  />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header:{
+      minHeight:93,
+      backgroundColor: "#2C4877",
+      paddingTop:40,
   },
 });
