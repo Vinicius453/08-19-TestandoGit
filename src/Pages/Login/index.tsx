8f5a5fc25ce4fb27fea3f7ab72ded015eac8a9db
Vinicius Karnopp <viniciuskarnopp777@gmail.com>
2021-09-26 20:11:09 -0400

    Tela de Login

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -9,4 +9,58 @@ const Login = () => {
     );
 }
 
+<<<<<<< HEAD
+=======
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "#4167b2",
+    },
+    conteudos: {
+        flex: 1,
+        justifyContent: "center",
+        alignContent: "center",
+    },
+    textInput: {
+        backgroundColor: "#ffffff",
+        marginTop: 30,
+        marginHorizontal: 40,
+        height: 48,
+        borderRadius: 5,
+        paddingLeft: 10,
+    },
+    facebook:{
+        justifyContent: "center",
+        //backgroundColor: "red",
+        alignItems: "center",
+        flexDirection: "row",
+        //marginTop: -80,
+    },
+    texto:{
+        fontSize: 60,
+        color: "#ffffff",
+        fontWeight:'bold',
+        marginLeft: 10,
+    },
+    email:{
+        marginTop: 50,
+    },
+    botao: {
+        fontSize: 25,
+        backgroundColor: "#223872",
+        marginTop: 30,
+        marginHorizontal: 40,
+        height: 48,
+        color: "#ffffff",
+        paddingTop: 5,
+        textAlign: "center",
+    },
+    texto2:{
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 150,
+    },
+});
+
+>>>>>>> parent of f5cc490... Tela de Login
 export default Login;
